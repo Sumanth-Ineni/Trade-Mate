3ac53e76f35829d27d4982ccbc75df5623498796
Sumanth-Ineni <sumanth.ivk@gmail.com>
2025-10-26 15:58:16 +0000

    Changed default port to 8080

--- server/index.ts
+++ server/index.ts
@@ -8,7 +8,7 @@ import apiRoutes = require('./routes');
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 8080;
 
 // Middleware
 app.use(cors()); // Enable Cross-Origin Resource Sharing
